3ffc2213f31beb1976dfdf04c92aebff967dbd50
Lucas Garcia <honzcha@outlook.com>
2020-08-30 02:03:51 +0800

    August 29

--- 02_node_backend/backend/src/config/upload.ts
+++ 02_node_backend/backend/src/config/upload.ts
@@ -1,6 +1,16 @@
-.idea/
-.vscode/
-node_modules/
-build/
-tmp/
-temp/
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+export default {
+  storage: multer.diskStorage({
+    // here __dirname is making the path until this file, than after that you go up one folder, than up another folder, than you go inside the tmp folder
+    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
--- 02_node_backend/backend/src/routes/users.routes.ts
+++ 02_node_backend/backend/src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,8 +26,12 @@ usersRouter.post('/', async (request, response) => {
 // using the method patch because it is updating/changing only one information of the user. PUT would is used to updated the entire user data
 
 // we are not doing the ensureAuthenticated for all the routes because if a user still needs to register, he will never be able to if it requires authentication, which is only given after user sign's up
-
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-  return response.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 export default usersRouter;
