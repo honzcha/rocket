b56a1aa46eaed60559e47c350f95633ed60b04a8
Lucas Garcia <honzcha@outlook.com>
2020-09-23 22:03:24 +0800

    September 23

--- 04_gobarber/appgobarber/src/pages/SignIn/index.tsx
+++ 04_gobarber/appgobarber/src/pages/SignIn/index.tsx
@@ -1,29 +1,73 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButtonText,
+  CreateAccountButton,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Login</Title>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Login</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Password" />
+            <Button
+              onPress={() => {
+                console.log('deu');
+              }}
+            >
+              Enter
+            </Button>
+            <ForgotPassword
+              onPress={() => {
+                console.log('forgot password button');
+              }}
+            >
+              <ForgotPasswordText>Forgot password</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Password" />
-      <Button
+      <CreateAccountButton
         onPress={() => {
-          console.log('deu');
+          console.log('fcreate account button');
         }}
       >
-        Enter
-      </Button>
-    </Container>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create Account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
